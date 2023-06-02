a0476f802db62d5325ebda6cc92269a07a9e7bbf
Akshat Jaiswal <akshatjaiswal.official@gmail.com>
2023-06-02 18:34:22 +0530

    done Header section.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { HiOutlineSearch } from "react-icons/hi";
 import { SlMenu } from "react-icons/sl";
@@ -16,34 +16,109 @@ const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
+  // As React is a SPA , so after redirecting to any other url we have to redirect it to top of that page,
+  // that's why we created this useEffect.
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  // window.scrollY tells the amount by which a page is scrolled
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("hide");
+      } else {
+        setShow("show");
+      }
+    } else {
+      setShow("top");
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => window.removeEventListener("scroll", controlNavbar);
+  }, [lastScrollY]);
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+      setTimeout(() => {
+        setShowSearch(false);
+      }, 1000);
+    }
+  };
   const openSearch = () => {
     setMobileMenu(false);
     setShowSearch(true);
   };
 
   const openMobileMenu = () => {
+    setMobileMenu(true);
+    setShowSearch(false);
+  };
+
+  const navigationhandler = (type) => {
+    if (type === "movie") {
+      navigate("/explore/movie");
+    } else if (type === "tv") {
+      navigate("/explore/tv");
+    }
     setMobileMenu(false);
-    setShowSearch(true);
   };
 
   return (
-    <header className={"header"}>
+    <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
       <ContentWrapper>
         <div className="logo">
           <img src={logo} alt={"logo"} />
         </div>
         <ul className={"menuItems"}>
-          <li className={"menuItem"}>Movies</li>
-          <li className={"menuItem"}>T.V. Shows</li>
+          <li
+            className={"menuItem"}
+            onClick={() => {
+              navigationhandler("movie");
+            }}
+          >
+            Movies
+          </li>
+          <li
+            className={"menuItem"}
+            onClick={() => {
+              navigationhandler("movie");
+            }}
+          >
+            T.V. Shows
+          </li>
           <li className={"menuItem"}>
-            <HiOutlineSearch />
+            <HiOutlineSearch onClick={openSearch} />
           </li>
         </ul>
-        <div className={"mobileMenuItems"}>
-          <HiOutlineSearch />
-          {mobileMenu ? <SlMenu /> : <VscChromeClose />}
+        <div className="mobileMenuItems">
+          <HiOutlineSearch onClick={openSearch} />
+          {mobileMenu ? (
+            <VscChromeClose onClick={() => setMobileMenu(false)} />
+          ) : (
+            <SlMenu onClick={openMobileMenu} />
+          )}
         </div>
       </ContentWrapper>
+      {showSearch && (
+        <div className={"searchBar"}>
+          <ContentWrapper>
+            <div className={"searchInput"}>
+              <input
+                type={"text"}
+                placeholder={"Search for a movie or T.V Show ..."}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyUp={searchQueryHandler}
+              />
+              <VscChromeClose onClick={() => setShowSearch(false)} />
+            </div>
+          </ContentWrapper>
+        </div>
+      )}
     </header>
   );
 };
