e7cee7991cc72440e40197b81293e1ba4e555090
Akshat Jaiswal <akshatjaiswal.official@gmail.com>
2023-06-02 15:01:55 +0530

    Updated Header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import ContentWrapper from "../contentWrapper/ContentWrapper.jsx";
+import logo from "../../../assets/movix-logo.svg";
+import "./Header.scss";
 
-function Header(props) {
-  return <div></div>;
-}
+const Header = () => {
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const openSearch = () => {
+    setMobileMenu(false);
+    setShowSearch(true);
+  };
+
+  const openMobileMenu = () => {
+    setMobileMenu(true);
+    setShowSearch(false);
+  };
+
+  return (
+    <header className={"header"}>
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} alt={"logo"} />
+        </div>
+        <ul className={"menuItems"}>
+          <li className={"menuItem"}>Movies</li>
+          <li className={"menuItem"}>T.V. Shows</li>
+          <li className={"menuItem"}>
+            <HiOutlineSearch />
+          </li>
+        </ul>
+        <div className={"mobileMenuItems"}>
+          <HiOutlineSearch />
+          {mobileMenu ? (
+            <SlMenu onClick={openMobileMenu} />
+          ) : (
+            <VscChromeClose onClick={() => setMobileMenu(false)} />
+          )}
+        </div>
+      </ContentWrapper>
+    </header>
+  );
+};
 
 export default Header;
